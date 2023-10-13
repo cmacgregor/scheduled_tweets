6bf8a187094abb46f646bca00661f061d69e210d
Connor MacGregor <macgregoco@gmail.com>
2023-10-13 16:50:20 -0500

    actually destory tweets when account disconnected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,3 @@
-#email:string
-#password_digest:string
-#
-#password:string virtual
-#password_confirmation:string virtual
-
 class User < ApplicationRecord
     has_many :twitter_accounts
     has_many :tweets
